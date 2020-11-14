4de9d89684eb6a72ae36378a25a6587d7792eda5
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-11-14 17:58:06 +0200

    Add project files.

---
+++ IntGreaterThan/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntGreaterThan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            int compNum = 0;
+            Console.WriteLine("Enter an integer to comare");
+            string compare = Console.ReadLine();
+            Int32.TryParse(compare, out compNum);
+
+            while (value <= compNum)
+            {
+                Console.Write("Enter an integer: ");
+
+                string line = Console.ReadLine();
+
+                Int32.TryParse(line, out value);
+            }
+
+            Console.WriteLine($"Integer greater than {compNum} detected!");
+
+            Console.ReadLine();
+        }
+    }
+}
